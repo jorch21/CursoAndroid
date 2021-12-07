0d246d1ac7aefd74f7a52b5f17019d801b753f28
Jorch21 <jorch303564@protonmail.com>
2021-12-06 22:02:52 -0500

    completando frmResumen

--- app/src/main/java/com/example/introduccion/frmResumen.kt
+++ app/src/main/java/com/example/introduccion/frmResumen.kt
@@ -2,10 +2,15 @@ package com.example.introduccion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_frm_resumen.*
 
 class frmResumen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frm_resumen)
+
+        val bundle:Bundle? = intent.extras
+        var vtNombre = bundle!!.getString("vtNombre")
+        lblResumen.text = vtNombre
     }
 }
